71c2ed01faa13718477220e860c048b7be060200
Mouwrice <mauricevanwassenhove@gmail.com>
2021-04-26 14:05:26 +0200

    Background now fills entire page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './App.css'
 import logo from './images/logo-white-on-transparent.png'
+import background from './images/background.png'
 
 function App () {
   return (
     <div>
-      <div className="background"/>
+      <img src={background} className="background" alt={background}/>
       <header>
         <img src={logo} id="logo" alt="logo"/>
         <ul id="languageList">
@@ -16,14 +17,18 @@ function App () {
       </header>
       <div id="main">
         <div id="consumers">
-          <h5>For consumers</h5>
-          <h3>Install the Stampix App and order prints in just a few clicks</h3>
-          <a href="https://stampix.com">START PRINTING</a>
+          <div>
+            <h5>For consumers</h5>
+            <h3>Install the Stampix App and order prints in just a few clicks</h3>
+            <a href="https://stampix.com">START PRINTING</a>
+          </div>
         </div>
         <div id="business">
-          <h5>For business</h5>
-          <h3>Build emotional loyalty and brand engagement</h3>
-          <a href="https://business.stampix.com">STAMPIX FOR BUSINESS</a>
+          <div>
+            <h5>For business</h5>
+            <h3>Build emotional loyalty and brand engagement</h3>
+            <a href="https://business.stampix.com">STAMPIX FOR BUSINESS</a>
+          </div>
         </div>
       </div>
     </div>
